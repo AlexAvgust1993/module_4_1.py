538a0d3bec9eaa71e23a31e43b2987806e2ba447
Alex <alexavgust1993@gmail.com>
2024-07-09 16:03:29 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import math
+import fake_math as op
+import true_math as li
+
+result1 = op.divide(69, 3)
+result2 = op.divide(3, 0)
+result3 = li.divide(49, 7)
+result4 = li.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
